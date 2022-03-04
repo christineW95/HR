f44e84f9051f7519977da55dfe1cd06adf2b01a0
Christina William <39514882+christineW95@users.noreply.github.com>
2022-03-04 23:25:22 +0200

    payslip

--- app/containers/Payslip/index.js
+++ app/containers/Payslip/index.js
@@ -1,43 +1,94 @@
-import { Button, TextareaAutosize } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import DatePicker from "react-datepicker";
+import React, { useEffect, useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import BasicDatePicker from '../../components/Datepicker';
-import RadioButtonsGroup from '../../components/RadioGroup';
-import MultipleSelect from '../../components/select';
-import UnstyledSelectSimple from '../../components/select';
-import EmptyTextarea from '../../components/TextArea';
-import TextInput from '../../components/TextInput';
+import BasicDatePicker from "../../components/Datepicker";
+import OutlinedCard from "../../components/Card";
+import Button from "@mui/material/Button";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 function Payslip() {
-    const [actualdate, setactualdate] = useState(null);
+  const [actualdate, setactualdate] = useState(null);
 
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        padding: 100,
+        flexDirection: "column",
+      }}
+    >
+      <h1>Employee Details</h1>
 
-    return (
-        <div    style={{
-            display: "flex",
-            flex: 1,
-            padding: 100,
-            flexDirection: "column",
-          }}
-      >
-                <h1>Employee Details</h1>
+      <BasicDatePicker
+        label={"Actual Date:"}
+        selected={actualdate}
+        onSelect={(date) => setactualdate(date)}
+        startDate={new Date()}
+        required
+        style={{}}
+      />
 
-<BasicDatePicker
-          label={"Actual Date:"}
-          selected={actualdate}
-          onSelect={(date) => setactualdate(date)}
-          startDate={new Date()}
-          required
-          style={{}}
-        />
-
-        <div style={{justifyContent:'center',display:'flex'}}>
-
-      <Button variant="contained" style={{ margin: 20,alignItems:'center' }}>View</Button>
+      <div style={{ justifyContent: "center", display: "flex" }}>
+        <Button
+          variant="contained"
+          style={{ margin: 20, alignItems: "center" }}
+        >
+          View
+        </Button>
       </div>
-
-        </div >
-    );
+      <div style={{ display: "flex", flex: 1, flexDirection: "row" ,justifyContent:'space-evenly'}}>
+        <OutlinedCard>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Total Pay
+          </Typography>
+          <Typography variant="h5" component="div">
+            15,000
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Net Balance
+          </Typography>
+          <Typography variant="h5" component="div">
+            15,000
+          </Typography>
+        </OutlinedCard>
+        <OutlinedCard>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Total Deductions
+          </Typography>
+          <Typography variant="h5" component="div">
+            15,000
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Net Balance
+          </Typography>
+          <Typography variant="h5" component="div">
+            15,000
+          </Typography>
+        </OutlinedCard>
+      </div>
+      <div style={{marginBlock:50}}>
+      <OutlinedCard>
+          <Typography sx={{ fontSize: 20,fontWeight:'bold' }} color="text.secondary" gutterBottom>
+Current Bank Account Details
+          </Typography>
+          <Typography variant="h7" component="div">
+            Bank Name
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Ahmed Zidan
+          </Typography>
+          <Typography variant="h7" component="div">
+            Account Number
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+123456489
+          </Typography>
+         </OutlinedCard>
+        
+      </div>
+    </div>
+  );
 }
 
 export default Payslip;
