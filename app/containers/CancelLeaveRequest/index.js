f7d65553617b4b6cca4e3ac254faf8279f663e76
Christina William <39514882+christineW95@users.noreply.github.com>
2022-03-04 23:07:13 +0200

    cancelrequest

---
+++ app/containers/CancelLeaveRequest/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Footer from "../../components/Footer";
+import BasicDatePicker from "../../components/Datepicker";
+import EmptyTextarea from "../../components/TextArea";
+import TextInput from "../../components/TextInput";
+import CustomList from "../../components/List";
+function CancelLeaveRequest() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+let listOfLeaves=[];
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        padding: 100,
+        flexDirection: "column",
+      }}
+    >
+                <h1>Current Leaves</h1>
+
+      <CustomList {...listOfLeaves}/>
+    
+      <Footer />
+    </div>
+  );
+}
+
+export default CancelLeaveRequest;
