a0fb53967d6a1f639a50234f2c5c5d7d87afdf4d
Christina William <39514882+christineW95@users.noreply.github.com>
2022-03-22 01:17:05 +0200

    leaveencash

--- app/containers/Payslip/index.js
+++ app/containers/Payslip/index.js
@@ -1,91 +1,117 @@
-import React, { useEffect, useState } from "react";
-import OutlinedCard from "../../components/Card";
-import Button from "@material-ui/core/Button";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import MaterialUIPickers from "../../components/Datepicker";
+import React, { useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import OutlinedCard from '../../components/Card';
+import MaterialUIPickers from '../../components/Datepicker';
 function Payslip() {
   const [actualdate, setactualdate] = useState(null);
+  const [show, setShow] = useState(false);
 
   return (
     <div
       style={{
-        display: "flex",
+        display: 'flex',
         flex: 1,
         padding: 100,
-        flexDirection: "column",
+        flexDirection: 'column',
       }}
     >
       <h1>Employee Details</h1>
 
       <MaterialUIPickers
-        label={"Actual Date:"}
+        label="Actual Date:"
         selected={actualdate}
-        onSelect={(date) => setactualdate(date)}
+        onSelect={date => setactualdate(date)}
         startDate={new Date()}
         required
         style={{}}
       />
 
-      <div style={{ justifyContent: "center", display: "flex" }}>
+      <div style={{ justifyContent: 'center', display: 'flex' }}>
         <Button
+          onClick={() => setShow(true)}
           variant="contained"
-          style={{ margin: 20, alignItems: "center" }}
+          color="secondary"
+          style={{ margin: 20, alignItems: 'center' }}
         >
           View
         </Button>
       </div>
-      <div style={{ display: "flex", flex: 1, flexDirection: "row" ,justifyContent:'space-evenly'}}>
-        <OutlinedCard>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Total Pay
-          </Typography>
-          <Typography variant="h5" component="div">
-            15,000
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            Net Balance
-          </Typography>
-          <Typography variant="h5" component="div">
-            15,000
-          </Typography>
-        </OutlinedCard>
-        <OutlinedCard>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Total Deductions
-          </Typography>
-          <Typography variant="h5" component="div">
-            15,000
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            Net Balance
-          </Typography>
-          <Typography variant="h5" component="div">
-            15,000
-          </Typography>
-        </OutlinedCard>
-      </div>
-      <div style={{marginBlock:50}}>
-      <OutlinedCard>
-          <Typography sx={{ fontSize: 20,fontWeight:'bold' }} color="text.secondary" gutterBottom>
-Current Bank Account Details
-          </Typography>
-          <Typography variant="h7" component="div">
-            Bank Name
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Ahmed Zidan
-          </Typography>
-          <Typography variant="h7" component="div">
-            Account Number
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-123456489
-          </Typography>
-         </OutlinedCard>
-        
-      </div>
+      {show ? (
+        <>
+          <div
+            style={{
+              display: 'flex',
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+            }}
+          >
+            <OutlinedCard>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Total Pay
+              </Typography>
+              <Typography variant="h5" component="div">
+                15,000
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Net Balance
+              </Typography>
+              <Typography variant="h5" component="div">
+                15,000
+              </Typography>
+            </OutlinedCard>
+            <OutlinedCard>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Total Deductions
+              </Typography>
+              <Typography variant="h5" component="div">
+                15,000
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Net Balance
+              </Typography>
+              <Typography variant="h5" component="div">
+                15,000
+              </Typography>
+            </OutlinedCard>
+          </div>
+          <div style={{ marginBlock: 50 }}>
+            <OutlinedCard>
+              <Typography
+                variant="h5"
+                sx={{ fontSize: 24, fontWeight: 'bold' }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Current Bank Account Details
+              </Typography>
+              <Typography variant="h7" component="div">
+                Bank Name
+              </Typography>
+              <Typography variant="h6" sx={{ mb: 1.5 }} color="text.secondary">
+                ABC
+              </Typography>
+              <Typography variant="h7" component="div">
+                Account Number
+              </Typography>
+              <Typography variant="h6" sx={{ mb: 1.5 }} color="text.secondary">
+                123456489
+              </Typography>
+            </OutlinedCard>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 }
